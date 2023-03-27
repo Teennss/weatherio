c0505ccee3093b5054c6d501a53e09f008f73226
Teenns <122209389+Teennss@users.noreply.github.com>
2023-03-27 21:13:08 +0900

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -2,7 +2,6 @@
 
 const api_key = "ee49293d4cf1aab571442bf396520abc";
 
-let api_key = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${ee49293d4cf1aab571442bf396520abc}`;
 /**
  *@param {string} URL API url
  *@param {Function} callback callback
@@ -21,16 +20,19 @@ export const url = {
   forecast(lat, lon) {
     return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`;
   },
+
   airPollution(lat, lon) {
-    return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`;
+    return `https://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`;
   },
+
   reverseGeo(lat, lon) {
-    return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`;
+    return `https://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`;
   },
+
   /**
    * @param {string} query
    **/
   geo(query) {
-    return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`;
+    return `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`;
   },
 };
